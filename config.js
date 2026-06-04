cff435c96b90bda81ce282f39dbd49c9c022c939
Tod <tpoirier@nmu.edu>
2026-06-04 09:38:31 -0400

    Enable Supabase configuration in config.js
    Uncomment Supabase configuration for camping.

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-window.CAMPING_SUPABASE_CONFIG = null;
-/* {
+window.CAMPING_SUPABASE_CONFIG = {
   url: 'https://wntakzfoprthwggkidyq.supabase.co',
   anonKey: 'sb_publishable_gWu_EQ1J3s0iNjeDeINJwQ_xKy8QgAJ',
   schema: 'camping'
 };
-*/
+
