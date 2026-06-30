0a5abc13d626c4ba9126741094047de249a3bf49
Tod <tpoirier@nmu.edu>
2026-06-30 10:24:55 -0400

    Add files via upload

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
-window.CAMPING_APP_VERSION = 'v23.1.57';
-window.CAMPING_APP_BUILD = 'v23.1.57';
-window.CAMPING_DATA_VERSION = 'v23.1.57-mn-finish1';
-window.CAMPING_DATA_BUILD = 'v23.1.57-mn-finish1';
-window.CAMPING_VERSION = 'v23.1.57';
-window.CAMPING_BUILD = { version: 'v23.1.57', build: 'v23.1.57', dataVersion: 'v23.1.57-mn-finish1', dataBuild: 'v23.1.57-mn-finish1', released: '2026-06-29', label: 'Boondocking & Camping Maps v23.1.57' };
+window.CAMPING_APP_VERSION = 'v23.1.58';
+window.CAMPING_APP_BUILD = 'v23.1.58';
+window.CAMPING_DATA_VERSION = 'v23.1.58-mn-west-closeout1';
+window.CAMPING_DATA_BUILD = 'v23.1.58-mn-west-closeout1';
+window.CAMPING_VERSION = 'v23.1.58';
+window.CAMPING_BUILD = { version: 'v23.1.58', build: 'v23.1.58', dataVersion: 'v23.1.58-mn-west-closeout1', dataBuild: 'v23.1.58-mn-west-closeout1', released: '2026-06-30', label: 'Boondocking & Camping Maps v23.1.58' };
