c0000d01d46da7404060920b0fb387600041ead9
Tod <tpoirier@nmu.edu>
2026-06-30 13:38:17 -0400

    Add files via upload

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
-window.CAMPING_APP_VERSION = 'v23.1.60';
-window.CAMPING_APP_BUILD = 'v23.1.60';
-window.CAMPING_DATA_VERSION = 'v23.1.60-mn-bulldog-ledger1';
-window.CAMPING_DATA_BUILD = 'v23.1.60-mn-bulldog-ledger1';
-window.CAMPING_VERSION = 'v23.1.60';
-window.CAMPING_BUILD = { version: 'v23.1.60', build: 'v23.1.60', dataVersion: 'v23.1.60-mn-bulldog-ledger1', dataBuild: 'v23.1.60-mn-bulldog-ledger1', released: '2026-06-30', label: 'Boondocking & Camping Maps v23.1.60 + MN Bulldog ledger' };
+window.CAMPING_APP_VERSION = 'v23.1.61';
+window.CAMPING_APP_BUILD = 'v23.1.61';
+window.CAMPING_DATA_VERSION = 'v23.1.61-mn-w23-super-audit1';
+window.CAMPING_DATA_BUILD = 'v23.1.61-mn-w23-super-audit1';
+window.CAMPING_VERSION = 'v23.1.61';
+window.CAMPING_BUILD = { version: 'v23.1.61', build: 'v23.1.61', dataVersion: 'v23.1.61-mn-w23-super-audit1', dataBuild: 'v23.1.61-mn-w23-super-audit1', released: '2026-06-30', label: 'Boondocking & Camping Maps v23.1.61 + MN Worker 2/3 supervisor audit' };
